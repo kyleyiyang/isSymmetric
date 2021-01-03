c1f52e661bdca47d149394fbf84573b504adc73a
kyleyiyang <kyleyiyang@gmail.com>
2021-01-03 15:37:20 -0500

    Update isSymmetric.java

--- isSymmetric.java
+++ isSymmetric.java
@@ -24,3 +24,25 @@ class Solution {
         return r1.val==r2.val && isMirror(r1.left,r2.right) && isMirror(r1.right, r2.left);
        }
 }
+
+//iterative
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1==null && t2==null) continue;
+            if (t1==null || t2==null) return false;
+            if (t1.val!=t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t2.left);
+            q.add(t1.right);
+        }
+        return true;
+    }
+}
